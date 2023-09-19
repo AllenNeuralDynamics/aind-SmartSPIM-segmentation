5fa58f86dd7af573c72f57c4782069e3db41a119
Nicholas Lusk <nicholas.lusk@alleninstitute.org>
2023-09-19 05:20:56 +0000

    Edited block_segmentation.py

--- code/src/aind_smartspim_segmentation/block_segmentation.py
+++ code/src/aind_smartspim_segmentation/block_segmentation.py
@@ -329,7 +329,7 @@ class Segment(ArgSchemaParser):
         if signal_end == -1:
             signal_end = signal_array.shape[-3]
 
-        signal_array = signal_array[0, 0, :300, :, :]
+        signal_array = signal_array[0, 0, :, :, :]
         logger.info("Truncating array, this is a TEST")
         logger.info(
             f"Starting detection with array {signal_array} with start in {signal_start} and end in {signal_end}"

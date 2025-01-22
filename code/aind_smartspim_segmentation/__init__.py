0414942bc9881ac5f4230f616f7b9abdbc29a31e
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-01-22 11:41:14 -0800

    updating list of authors

--- code/aind_smartspim_segmentation/__init__.py
+++ code/aind_smartspim_segmentation/__init__.py
@@ -3,8 +3,14 @@ Module init file
 """
 
 __version__ = "0.0.7"
-__author__ = "Camilo Laiton"
-__author_email__ = "camilo.laiton@alleninstitute.org"
+__authors__ = [
+    "Camilo Laiton",
+    "Nicholas Lusk"
+]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+    "nicholas.lusk@alleninstitute.org"
+]
 __license__ = "MIT"
 __description__ = (
     "This version uses the spot detection algorithm from "
@@ -14,7 +20,10 @@ __description__ = (
 )
 __url__ = "https://github.com/AllenNeuralDynamics/aind-SmartSPIM-segmentation"
 
-__maintainer__ = "Camilo Laiton"
-__maintainer_email__ = "camilo.laiton@alleninstitute.org"
+__maintainers__ = ["Camilo Laiton", "Nicholas Lusk"]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+    "nicholas.lusk@alleninstitute.org"
+]
 __title__ = "aind-smartspim-cell-proposals"
 __status__ = "Development"  # 'Production', 'Beta'

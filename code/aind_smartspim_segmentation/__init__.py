240d26a438e208ba2fa013da26d75cf51aedf04a
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-01-30 19:51:08 -0800

    updating neuroglancer link and running linters

--- code/aind_smartspim_segmentation/__init__.py
+++ code/aind_smartspim_segmentation/__init__.py
@@ -1,2 +1,19 @@
 """Init file for cell segmentation"""
+
 __version__ = "0.0.6"
+__authors__ = ["Nicholas Lusk", "Camilo Laiton"]
+__author_emails__ = [
+    "nicholas.lusk@alleninstitute.org",
+    "camilo.laiton@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = "This version detects cell proposals using cellfinder's algorithm "
+__url__ = "https://github.com/AllenNeuralDynamics/aind-SmartSPIM-segmentation"
+__maintainers__ = ["Nicholas Lusk", "Camilo Laiton"]
+__maintainer_emails__ = [
+    "nicholas.lusk@alleninstitute.org",
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-SmartSPIM-segmentation"
+__status__ = "Production"  # 'Production', 'Beta'
+__pipeline_version__ = "3.0.0"
